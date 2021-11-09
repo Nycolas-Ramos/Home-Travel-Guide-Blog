9d35d19e06b1053776fe50c55be137417454e756
Sasori <nycsenju@gmail.com>
2021-11-09 09:36:46 -0300

    Animação header e Começo do explore dream

--- js/home.js
+++ js/home.js
@@ -2,13 +2,21 @@
 const header = document.getElementById('header')
 
 function scrollHeader(){
-  const windowTop =window.innerHeight * 0.95
-
+  const windowTop =window.innerHeight * 0.95 - 60
   const headerTop = window.scrollY + 80
+  
   if (windowTop < headerTop){
-    console.log(windowTop)
+    if(!header.classList.contains('hide')){
+      header.classList.add('hide')
+      header.classList.remove('show')
+    }
+  }else if (windowTop > headerTop){
+    if(header.classList.contains('hide')){
+      header.classList.add('show')
+      header.classList.remove('hide')
+    }
   }
 }
 window.addEventListener('scroll', scrollHeader)
-
 // Animacao do cabecalho
+
